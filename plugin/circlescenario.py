00f3af28962f9bd59b77df07ce28461f655ce914
stmoon <munhoney@gmail.com>
2017-06-26 23:49:07 +0900

    implement circle (NOT WORKING)

--- plugin/circlescenario.py
+++ plugin/circlescenario.py
@@ -14,5 +14,19 @@ class CircleScenario (IScenarioPlugin) :
 	self._properties['trans_end_point'] = [0,0,0]
 
     def update(self) :
-	pass
 
+        start_point = np.array(self._properties['start_point'], dtype='float32')
+        end_point = np.array(self._properties['end_point'], dtype='float32')
+        rate = self._properties['rate']
+        start_time = self._properties['start_time']
+        end_time = self._properties['end_time']
+        duration = end_time - start_time
+	
+	# init
+	np.array([[
+	
+	# rotation 
+        for i in xrange(int(rate*duration)) :
+            time = start_time + i * (1.0/rate)
+
+            #self._nodes[self._node].append([time, traj[0], traj[1], traj[2]])
